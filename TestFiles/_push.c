3052f38c5951b05b220793b0d3e6567620db7654
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2023-11-16 23:04:13 +0300

    testing

---
+++ TestFiles/_push.c
@@ -0,0 +1,50 @@
+#include "monty.h"
+
+/**
+ * handle_error - Helper function to handle errors
+ * @line_number: Int line
+ * @head: Head
+ */
+void handle_error(unsigned int line_number, stack_t **head)
+{
+    fprintf(stderr, "L%d: usage: push integer\n", line_number);
+    fclose(env.file);
+    free(env.content);
+    free_stack(*head);
+    exit(EXIT_FAILURE);
+}
+
+/**
+ * _push - Func add node
+ * @head: Head
+ * @line_number: Int line
+ */
+void _push(stack_t **head, unsigned int line_number)
+{
+    size_t j;
+    int y;
+
+    if (!env.arg || !*env.arg)
+    {
+        handle_error(line_number, head);
+    }
+
+    for (j = (env.arg[0] == '-'); env.arg[j]; ++j)
+    {
+        if (!isdigit(env.arg[j]))
+        {
+            handle_error(line_number, head);
+        }
+    }
+
+    y = atoi(env.arg);
+
+    if (env.lifi == 0)
+    {
+        addNode(head, y);
+    }
+    else
+    {
+        addQueue(head, y);
+    }
+}
